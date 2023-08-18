2007f1023bbfa53285663a67bdd964013b377372
samAricha <samaricha001@gmail.com>
2023-08-18 23:51:16 +0300

    top bar names

--- lib/dashboard_screen.dart
+++ lib/dashboard_screen.dart
@@ -40,7 +40,7 @@ class _DashboardScreenState extends State<DashboardScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Dashboard'),
+        title: const Text('CPIMS'),
       ),
       body: SingleChildScrollView(
         padding: const EdgeInsets.all(16.0),
